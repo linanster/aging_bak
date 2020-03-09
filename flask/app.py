3151650a221844453470d70bb55fef90f5d90eb6
LiNan <believelinan@aliyun.com>
2020-03-09 17:28:25 +0800

    support start,changemesh,scan

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,17 @@
 #! /usr/bin/env python3
 # coding:utf8
 #
-from flask import Flask, request, render_template
-import json
+from flask import Flask, request, render_template, flash, redirect, url_for
+import json, time
 from lib import mydb
 from lib import mycmd
 
-
+STOP = False
+STOPPED = False
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "random string"
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index/', methods=['GET', 'POST'])
@@ -36,24 +39,61 @@ def handle_info_factory():
 
 @app.route('/cmd_start/', methods=['POST'])
 def handle_cmd_start():
+    global STOP
+    global STOPPED
+    # 1.start
     errno = mycmd.start()
-    if errno == 0:
-        # return json.loads('{"cmd":"start","result":"success"}')
-        return json.dumps({"cmd":"start","result":"success"})
-    else:
-        # return json.loads('{"cmd":"start","result":"failed","errno":%d}' % errno)
+    if errno != 0:
         return json.dumps({"cmd":"start","result":"failed","errno":errno})
+    time.sleep(60) 
+    # 2.change mesh
+    loop = 4
+    while loop > 0:
+        errno = mycmd.changemesh()
+        if errno != 0:
+            return json.dumps({"cmd":"changemesh","result":"failed","errno":errno})
+        time.sleep(10)
+        loop = loop -1
+    # 3.scan
+    while not STOP:
+        errno = mycmd.scan()
+        if errno != 0:
+            return json.dumps({"cmd":"scan","result":"failed","errno":errno})
+        time.sleep(10)        
+    STOPPED = True
+    return redirect(url_for('handle_info_aging'))
+
+
+@app.route('/cmd_changemesh/')
+def handle_cmd_changemesh():
+    errno = mycmd.changemesh()
+    if errno == 0:
+        # return json.loads('{"cmd":"changemesh","result":"success"}')
+        return json.dumps({"cmd":"changemesh","result":"success"})
+    else:
+        # return json.loads('{"cmd":"changemesh","result":"failed","errno":%d}' % errno)
+        return json.dumps({"cmd":"changemesh","result":"failed","errno":errno})
+
+@app.route('/cmd_scan/')
+def handle_cmd_scan():
+    errno = mycmd.scan()
+    if errno == 0:
+        # return json.loads('{"cmd":"scan","result":"success"}')
+        return json.dumps({"cmd":"scan","result":"success"})
+    else:
+        # return json.loads('{"cmd":"scan","result":"failed","errno":%d}' % errno)
+        return json.dumps({"cmd":"scan","result":"failed","errno":errno})
 
 @app.route('/cmd_stop/', methods=['POST'])
 def handle_cmd_stop():
-    errno = mycmd.stop()
-    if errno == 0:
-        # return json.loads('{"cmd":"stop","result":"success"}')
-        return json.dumps({"cmd":"stop","result":"success"})
-    else:
-        # return json.loads('{"cmd":"stop","result":"failed","errno":%d}' % errno)
-        return json.dumps({"cmd":"stop","result":"failed","errno":errno})
-
+    global STOP
+    global STOPPED
+    STOP = True
+    while not STOPPED:
+        time.sleep(1)
+    flash('Stopped success!')
+    return redirect(url_for('handle_index'))
+  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True, threaded=True)
--- flask/lib/mycmd.py
+++ flask/lib/mycmd.py
@@ -5,22 +5,48 @@ import time
 import os
 import subprocess
 
+DEBUG = False
+program = "./ble-backend-nan" if DEBUG else "./ble-backend"
+
 def start():
     try:
         print('starting...')
-        time.sleep(3)
+        time.sleep(1)
         # print os.getcwd() - /git/aging/flask
-        # p = subprocess.Popen("./ble-backend-nan --scan", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
-        p = subprocess.Popen("./ble-backend -command=scan", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
-        # 模拟错误
-        # raise Error
+        # p = subprocess.Popen("./ble-backend-nan --command=start", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        # p = subprocess.Popen("./ble-backend -command=start", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        p = subprocess.Popen("{} -command=start".format(program), shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
     except Exception as e:
-        print(str(e))
+        print("start error:",str(e))
         return 1
     else:
         print("start success")
         return 0
+
+def changemesh():
+    try:
+        # p = subprocess.Popen("./ble-backend-nan -command=changemesh", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        # p = subprocess.Popen("./ble-backend -command=changemesh", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        p = subprocess.Popen("{} -command=changemesh".format(program), shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+    except Exception as e:
+        print("change mesh error:", str(e))
+        return 1
+    else:
+        print("change mesh success")
+        return 0
     
+def scan():
+    try:
+        # p = subprocess.Popen("./ble-backend-nan -command=scan", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        # p = subprocess.Popen("./ble-backend -command=scan", shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+        p = subprocess.Popen("{} -command=scan".format(program), shell=True, close_fds=True, stdin=subprocess.PIPE, stdout=None)
+    except Exception as e:
+        print("scan error:", str(e))
+        return 1
+    else:
+        print("scan success")
+        return 0
+
 def stop():
     try:
         print ("stopping...")
