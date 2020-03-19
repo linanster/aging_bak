aeeff3dcc4fa4491766422d92fa96f9cde49b9ac
LiNan <believelinan@aliyun.com>
2020-03-19 20:27:25 +0800

    app/__init__.py: from app.views import init_views

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@
 # coding:utf8
 #
 from flask import Flask
-from .models import init_models
-from .views import init_views
-from .settings import config
-from .ext import init_ext
+from app.models import init_models
+from app.views import init_views
+from app.settings import config
+from app.ext import init_ext
 
 def create_app():
     app = Flask(__name__)
