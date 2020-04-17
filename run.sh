a65acaed237a13ceb92607a4174b093effe8c451
LiNan <believelinan@aliyun.com>
2020-04-18 02:06:22 +0800

    colorize run.sh output

--- run.sh
+++ run.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 #
-set -o errexit
+# set -o errexit
+
+
+# lib: color print
+bold=$(tput bold)
+green=$(tput setf 2)
+red=$(tput setf 4)
+reset=$(tput sgr0)
+
+function green() {
+          printf "${bold}${green}%s${reset}\n" "$@";
+  }
+function red() {
+          printf "${bold}${red}%s${reset}\n" "$@";
+  }
+
+
+usage=$"
+Usage: run.sh [--start] [--stop] [--status] [--init]
+"
 
 if [ $# -eq 0 ]; then
-    echo "run.sh [--start] [--stop] [--status] [--init]"
+    red "${usage}"
     exit 1
 fi
 if [ "$1" != "--start" -a "$1" != "--stop" -a "$1" != "--status" -a "$1" != "--init" -a "$1" != "--upload" -a "$1" != "--purge" ]; then
-    echo "run.sh [--start] [--stop] [--status] [--init] [--upload] [--purge]"
+    red "${usage}"
     exit 1
 fi
 
@@ -20,10 +39,10 @@ if [ "$1" == "--init" ]; then
     source ./venv/bin/activate
     pip3 install -r requirements.txt
     if [ $? -eq 0 ]; then
-        echo "==init config complete=="
+        green "==init config complete=="
         exit 0
     else
-        echo "==init config fail=="
+        red "==init config fail=="
         exit 1
     fi
 fi
@@ -31,14 +50,14 @@ fi
 if [ -d venv ]; then
     source ./venv/bin/activate
 else
-    echo "==venv error=="
+    red "==venv error=="
     exit 1
 fi
 
 # cd "$workdir/app"
 
 if [ "$1" == '--start' ]; then
-    echo "gunicorn --workers 1 --bind 0.0.0.0:5000 --timeout 300 --worker-class eventlet wsgi:application_ge_aging"
+    green "gunicorn --workers 1 --bind 0.0.0.0:5000 --timeout 300 --worker-class eventlet wsgi:application_ge_aging"
     # todo --daemon
     # todo --user user1 --group user1
     if [ "$2" == '--nodaemon' ]; then
@@ -53,9 +72,9 @@ fi
 if [ "$1" == "--stop" ]; then
     pid=$(ps -ef | fgrep "gunicorn" | grep "application_ge_aging" | awk '{if($3==1) print $2}')
     if [ "$pid" == "" ]; then
-        echo "not running" 
+        red "not running" 
     else
-        echo "kill $pid"
+        green "kill $pid"
         kill "$pid"
     fi
     exit 0
@@ -63,21 +82,21 @@ fi
 
 if [ "$1" == "--status" ]; then
     pid=$(ps -ef | fgrep "gunicorn" | grep "application_ge_aging" | awk '{if($3==1) print $2}')
-    echo "$pid"
     if [ "$pid" == "" ]; then
-        echo "stopped" 
+        red "stopped" 
     else
-        echo "started"
+        green "$pid"
+        green "started"
     fi
     exit 0
 fi
 
 if [ "$1" == "--upload" ]; then
     python3 manage.py upload
-    echo $? 
+    green "$?" 
 fi
 
 if [ "$1" == "--purge" ]; then
     python3 manage.py purge
-    echo $? 
+    green "$?" 
 fi
