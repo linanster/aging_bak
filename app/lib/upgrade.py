23746bfacb0245c6dc8fd4e25de8d1007e42cbd1
LiNan <believelinan@aliyun.com>
2020-04-21 01:53:13 +0800

    implement check_github_connection func

--- app/lib/upgrade.py
+++ app/lib/upgrade.py
@@ -1,8 +1,33 @@
+import requests
+from requests.packages.urllib3 import disable_warnings
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+disable_warnings(InsecureRequestWarning)
 
 
 def check_github_connection():
-    pass
+    method = 'GET'
+    ############################################
+    url = "https://github.com"
+    ############################################
+    headers = {}
+    payload = {}
+    try:
+        response = requests.request(method=method, url=url, headers=headers, data=payload, timeout=10, verify=False)
+    except Exception as e:
+        print(str(e))
+        return False
+    else:
+        if response.ok:
+            return True
+        else:
+            return False
 
+
+
+def check_upgrade_pin():
+    pass
 
 def exec_upgrade():
     pass
