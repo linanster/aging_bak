300fa6e7fe6262e18567c7d6834e937fe83ad15e
LiNan <believelinan@aliyun.com>
2020-04-21 15:55:50 +0800

    change popen shell=False

--- app/lib/upgrade.py
+++ app/lib/upgrade.py
@@ -3,6 +3,8 @@ from requests.packages.urllib3 import disable_warnings
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import subprocess
 
+from .mylogger import logger_app
+
 from app.myglobal import topdir
 
 disable_warnings(InsecureRequestWarning)
@@ -31,6 +33,19 @@ def check_github_connection():
 def check_upgrade_pin():
     pass
 
+
 def exec_upgrade():
-    errno = subprocess.call("./run.sh --upgrade", shell=True, cwd=topdir)
+    # errno = subprocess.call("./run.sh --upgrade", shell=True, cwd=topdir)
+    # return errno
+    p = subprocess.Popen(["./run.sh", "--upgrade"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=topdir)
+    while p.poll() is None:
+        output = p.stdout.readline().decode('utf-8')[0:-1]
+        logger_app.info('[upgrade] {}'.format(output))
+    errno = p.poll()
+    if errno != 0:
+        errmsg = p.stderr.read().decode('utf-8')[:-1]
+        logger_app.error('[upgrade] {}'.format(errmsg))
+        logger_app.error('[upgrade] errno:{}'.format(errno))
+    p.stdout.close()
+    p.stderr.close()
     return errno
