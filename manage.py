c8177b16f32f23346a777bef65c223a43db3a3a1
LiNan <believelinan@aliyun.com>
2020-05-21 15:16:36 +0800

    update manage.py: copy template functions from wsgi.py [*]

--- manage.py
+++ manage.py
@@ -104,6 +104,74 @@ def purge():
     return purge_local_archive()
 
 
+@app.template_global('devicename')
+def devicename():
+    from app.lib import get_devicename
+    return get_devicename()
+
+@app.template_global('devicecode')
+def devicecode():
+    from app.lib import get_devicecode
+    return get_devicecode()
+
+@app.template_global('totalcount')
+def totalcount():
+    from app.lib import get_totalcount
+    return get_totalcount()
+
+@app.template_global('fwversion')
+def fwversion():
+    from app.lib import get_fwversion
+    return get_fwversion()
+
+@app.template_global('mcuversion')
+def mcuversion():
+    from app.lib import get_mcuversion
+    return get_mcuversion()
+
+@app.template_global('progress')
+def progress():
+    from app.lib import get_progress
+    return get_progress()
+
+@app.template_global('running')
+def running():
+    from app.lib import get_running_state
+    return get_running_state()
+
+@app.template_global('phase')
+def phase():
+    from app.lib import get_phase
+    return get_phase()
+
+@app.template_global('errno')
+def errno():
+    from app.lib import get_errno
+    return get_errno()
+
+@app.template_filter('parse_is_qualified')
+def parseIsQualified(mybool):
+    return '成功' if mybool else '失败'
+
+@app.template_filter('parse_status')
+def parseStatusCode(code):
+    if '2' in str(code):
+        plaintxt = '失败'
+    else:
+        plaintxt = '成功'
+    return '{}({})'.format(plaintxt,code)
+
+@app.template_filter('parse_status_boolean')
+def parseStatusCodeBoolean(code):
+    if '2' in str(code):
+        statusbool = False
+    else:
+        statusbool = True
+    return statusbool
+
+
+
+
 if __name__ == '__main__':
     manager.run()
 
