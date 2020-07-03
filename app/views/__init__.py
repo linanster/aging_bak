82937b27d55b15b342f1d38e178978502b910e1a
LiNan <believelinan@aliyun.com>
2020-07-03 14:41:36 +0800

    remove views/blue_admin.py

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,11 +3,9 @@ def init_views(app):
     from .blue_test import blue_test
     from .blue_manage import blue_manage
     from .blue_about import blue_about
-    from .blue_admin import blue_admin
     from .blue_control import blue_control
     app.register_blueprint(blue_index)
     app.register_blueprint(blue_test)
     app.register_blueprint(blue_manage)
     app.register_blueprint(blue_about)
-    app.register_blueprint(blue_admin)
     app.register_blueprint(blue_control)
