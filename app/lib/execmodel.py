11d5535473c73785378bfab5d11e06f5fe65a3c3
LiNan <believelinan@aliyun.com>
2020-09-21 15:40:05 +0800

    execmodel.py: enhance testdatas_archive

--- app/lib/execmodel.py
+++ app/lib/execmodel.py
@@ -44,34 +44,40 @@ def testdatasstage_cleanup_archived():
 
 # copy data from testdatas to testdatasstage table
 def testdatas_stage():
-    testdatas_list = Testdata.query.all()
-    testdatasstage_list = list()
-    for item in testdatas_list:
-        devicecode = item.devicecode
-        factorycode = item.factorycode
-        fw_version = item.fw_version
-        rssi_ble1 = item.rssi_ble1
-        rssi_ble2 = item.rssi_ble2
-        rssi_wifi1 = item.rssi_wifi1
-        rssi_wifi2 = item.rssi_wifi2
-        mac_ble = item.mac_ble
-        mac_wifi = item.mac_wifi
-        status_cmd_check1 = item.status_cmd_check1
-        status_cmd_check2 = item.status_cmd_check2
-        bool_uploaded = item.bool_uploaded
-        bool_qualified_signal = item.bool_qualified_signal
-        bool_qualified_check = item.bool_qualified_check
-        bool_qualified_scan = item.bool_qualified_scan
-        bool_qualified_deviceid = item.bool_qualified_deviceid
-        datetime = item.datetime
-        reserve_int_1 = item.reserve_int_1
-        reserve_str_1 = item.reserve_str_1
-        reserve_bool_1 = item.reserve_bool_1
-        bool_qualified_overall = item.bool_qualified_overall
-        obj = TestdataStage(devicecode, factorycode, fw_version, rssi_ble1, rssi_ble2, rssi_wifi1, rssi_wifi2, mac_ble, mac_wifi, status_cmd_check1, status_cmd_check2, bool_uploaded, bool_qualified_signal, bool_qualified_check, bool_qualified_scan, bool_qualified_deviceid, datetime, reserve_int_1, reserve_str_1, reserve_bool_1, bool_qualified_overall)
-        testdatasstage_list.append(obj)
-    db_mysql.session.add_all(testdatasstage_list)
-    db_mysql.session.commit()
+    try:
+        testdatas_list = Testdata.query.all()
+        testdatasstage_list = list()
+        for item in testdatas_list:
+            devicecode = item.devicecode
+            factorycode = item.factorycode
+            fw_version = item.fw_version
+            rssi_ble1 = item.rssi_ble1
+            rssi_ble2 = item.rssi_ble2
+            rssi_wifi1 = item.rssi_wifi1
+            rssi_wifi2 = item.rssi_wifi2
+            mac_ble = item.mac_ble
+            mac_wifi = item.mac_wifi
+            status_cmd_check1 = item.status_cmd_check1
+            status_cmd_check2 = item.status_cmd_check2
+            bool_uploaded = item.bool_uploaded
+            bool_qualified_signal = item.bool_qualified_signal
+            bool_qualified_check = item.bool_qualified_check
+            bool_qualified_scan = item.bool_qualified_scan
+            bool_qualified_deviceid = item.bool_qualified_deviceid
+            datetime = item.datetime
+            reserve_int_1 = item.reserve_int_1
+            reserve_str_1 = item.reserve_str_1
+            reserve_bool_1 = item.reserve_bool_1
+            bool_qualified_overall = item.bool_qualified_overall
+            obj = TestdataStage(devicecode, factorycode, fw_version, rssi_ble1, rssi_ble2, rssi_wifi1, rssi_wifi2, mac_ble, mac_wifi, status_cmd_check1, status_cmd_check2, bool_uploaded, bool_qualified_signal, bool_qualified_check, bool_qualified_scan, bool_qualified_deviceid, datetime, reserve_int_1, reserve_str_1, reserve_bool_1, bool_qualified_overall)
+            testdatasstage_list.append(obj)
+        db_mysql.session.add_all(testdatasstage_list)
+
+    except Exception as e:
+        logger_app.error('testdatas_stage:')
+        logger_app.error(str(e))
+    else:
+        db_mysql.session.commit()
 
 
 # copy data from testdatasstage to testdatasarchive table
